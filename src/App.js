c1f2e8b5738d795e34cf0d6675a93c4778cb549e
Rohit Nayak <rohitnayak7860@gmail.com>
2024-05-10 16:26:40 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        Developed by Rohit Nayak
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  key: "15437dbf343431af912d70b3515d1481",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
